db6cba5b2d6824f8aa0f86a2eaba6cb0b31c2e17
Gerbull <da.geraskin@gmail.com>
2024-10-09 10:03:59 +0200

    After Highlighting current side navigation link

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'bttrgjndzhjamkrzbtys.supabase.co', // 1st part of the images url
+				port: '',
+				pathname: '/storage/v1/object/public/cabin-images/**', // 2nd part
+			},
+		],
+	},
+	// output: 'export',
+};
 
 export default nextConfig;
